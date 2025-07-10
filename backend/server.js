cafc2b976c547f57f982a72a787a2a83f03cc03b
Shivam <alvindevil568@gmail.com>
2025-07-11 00:48:51 +0530

    safe check endpoint

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ const path = require('path');
 // TEST COMMIT to back safe commit before major changes  --- 2
 // TEST COMMIT to back safe commit before major changes  --- 3
 // TEST COMMIT to back safe commit before major changes  --- 4
+// TEST COMMIT to back safe commit before major changes  --- 5
 
 // Import routes
 const authRoutes = require('./routes/authRoutes');
